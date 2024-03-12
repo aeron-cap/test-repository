5cf0688382ce2d536138791d2033e38e69b1d24a
aeron-cap <aeroncaponpon.01@gmail.com>
2024-03-12 14:10:44 +0800

    Reapply "COMPANIES - ADD,UPDATE,DELETE"
    This reverts commit 003707fdfb1296ff935474df7cd0e28cbf4f8a7b.

--- front-end/src/pages/ViewCompany/Heading.jsx
+++ front-end/src/pages/ViewCompany/Heading.jsx
@@ -1,15 +1,11 @@
-import { Button, HStack, Heading, Spacer } from "@chakra-ui/react";
+import { HStack, Heading, Spacer } from "@chakra-ui/react";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <HStack paddingTop={10}>
       <Heading textAlign="center">Company</Heading>
       <Spacer />
-      <Button as={Link} to="/companies/add" colorScheme="green">
-        Add Resource
-      </Button>
     </HStack>
   );
 };
